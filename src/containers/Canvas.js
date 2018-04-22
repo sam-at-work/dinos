1283c59c88bfd780fce3648fdad9d4a1933ed78a
Sam Grace <sam@festicket.com>
2018-04-22 18:38:44 +0100

    refactored

--- src/containers/Canvas.js
+++ src/containers/Canvas.js
@@ -8,88 +8,86 @@ const Canvas = styled.canvas`
   bottom: 0;
   left: 0;
   right: 0;
-  //background-color: red;
 `;
 
 export default class DinoCanvas extends React.Component {
+  ctx; // set in componentDidMount;
 
-  constructor() {
-    super()
-  }
+  //create the array of circles that will be animated
+  dinos = Object.values(dinoSVGs).map(path => {
+    const base_image = new Image();
+    base_image.src = path;
+    return {
+      image: base_image,
+      vx: Math.random() * 1,
+      vy: Math.random() * 1,
+    };
+  });
+
+  //create the container that will hold the bouncing balls.
+  container = { x: 0, y: 0 };
+
+  setCanvasSize = () => {
+    const canvas = this.canvas;
+    const container = this.container;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    container.width = canvas.width;
+    container.height = canvas.height;
+
+    this.dinos.forEach(dino => {
+      if (!dino.x || dino.x > container.width - dino.image.width) {
+        dino.x = Math.floor(Math.random() * (container.width - dino.image.width));
+      }
+      if (!dino.y || dino.y > container.height - dino.image.height) {
+        dino.y = Math.floor(Math.random() * (container.height - dino.image.height));
+      }
+    });
+  };
+
+  animate = () => {
+    const ctx = this.ctx;
+    const dinos = this.dinos;
+    const container = this.container;
+
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    dinos.forEach(dino => {
+      ctx.drawImage(dino.image, dino.x, dino.y);
+
+      //time to animate our circles ladies and gentlemen.
+      if (
+        dino.x + dino.vx < container.x ||
+        dino.x + dino.image.width + dino.vx > container.x + container.width
+      ) {
+        dino.vx = -dino.vx;
+      }
+
+      if (
+        dino.y + dino.vy < container.y ||
+        dino.y + dino.image.height + dino.vy > container.y + container.height
+      ) {
+        dino.vy = -dino.vy;
+      }
+
+      dino.x += dino.vx;
+      dino.y += dino.vy;
+    });
+    requestAnimationFrame(this.animate);
+  };
 
   componentDidMount() {
-    const canvas = this.canvas;
-    if (canvas.getContext) {
-      //create the array of circles that will be animated
-      const dinos = Object.values(dinoSVGs).map(path => {
-        const base_image = new Image();
-        base_image.src = path;
-        return {
-          image: base_image,
-          vx: Math.random() * 2,
-          vy: Math.random() * 2
-        };
-      });
+    if (this.canvas.getContext) {
+      this.ctx = this.canvas.getContext("2d");
 
-      //create the container that will hold the boincing balls.
-      const container = {
-        x: 0,
-        y: 0
+      window.addEventListener("resize", this.setCanvasSize);
+
+      this.dinos[this.dinos.length - 1].image.onload = () => {
+        this.setCanvasSize();
+        requestAnimationFrame(this.animate);
       };
-
-      function setCanvasSize() {
-        const canvasWidth = window.innerWidth;
-        const canvasHeight = window.innerHeight;
-        canvas.width = canvasWidth;
-        canvas.height = canvasHeight;
-
-        dinos.forEach(dino => {
-          dino.x = Math.floor(Math.random() * (canvasWidth - dino.image.width));
-          dino.y = Math.floor(
-            Math.random() * (canvasHeight - dino.image.height)
-          );
-        });
-
-        container.width = canvasWidth;
-        container.height = canvasHeight;
-      }
-
-      window.addEventListener("resize", setCanvasSize);
-
-      const ctx = canvas.getContext("2d");
-
-      dinos[dinos.length - 1].image.onload = function() {
-        setCanvasSize();
-        requestAnimationFrame(animate);
-      };
-
-      function animate() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        dinos.forEach(dino => {
-          ctx.drawImage(dino.image, dino.x, dino.y);
-
-          //time to animate our circles ladies and gentlemen.
-          if (
-            dino.x + dino.vx < container.x ||
-            dino.x + dino.image.width + dino.vx > container.x + container.width
-          ) {
-            dino.vx = -dino.vx;
-          }
-
-          if (
-            dino.y + dino.vy < container.y ||
-            dino.y + dino.image.height + dino.vy >
-              container.y + container.height
-          ) {
-            dino.vy = -dino.vy;
-          }
-
-          dino.x += dino.vx;
-          dino.y += dino.vy;
-        });
-        requestAnimationFrame(animate);
-      }
     }
   }
 
@@ -99,9 +97,7 @@ export default class DinoCanvas extends React.Component {
         innerRef={canvas => {
           this.canvas = canvas;
         }}
-      >
-        Test
-      </Canvas>
+      />
     );
   }
 }
--- src/containers/SectionOne.js
+++ src/containers/SectionOne.js
@@ -14,6 +14,13 @@ const Wrapper = styled.div`
   margin-right: auto;
   margin-left: auto;
 
+  position: relative; // needed to make dino canvas appear behind it;
+  
+  .text-wrapper {
+    margin-left: 30px;
+    margin-right: 30px;
+  }
+
   .vertical-separator {
     background-color: rgb(241, 129, 230);
     height: 5px;
@@ -25,18 +32,20 @@ export default function SectionOne() {
     <React.Fragment>
       <DinoCanvas />
       <Wrapper>
-        <H1>find the perfect dino for you.</H1>
-        <div className="vertical-separator" />
-        <div>
-          <p>We all have dinosaurs deep within us just trying to get out.</p>
-          <p>
-            There is a dinosaurian connection. Our love of dinosaurs is a little
-            more intense than is typical. By adopting your dino, you’re
-            supporting our fresh discoveries. Our dinosaurs are big, fierce and
-            lovely. This makes them suitably impressive, but safe to approach.
-            There’s no risk of Allosaurus snaffling up kids who lean over the
-            barriers at the Natural History Museum.
-          </p>
+        <div className="text-wrapper">
+          <H1>find the perfect dino for you.</H1>
+          <div className="vertical-separator" />
+          <div>
+            <p>We all have dinosaurs deep within us just trying to get out.</p>
+            <p>
+              There is a dinosaurian connection. Our love of dinosaurs is a
+              little more intense than is typical. By adopting your dino, you’re
+              supporting our fresh discoveries. Our dinosaurs are big, fierce
+              and lovely. This makes them suitably impressive, but safe to
+              approach. There’s no risk of Allosaurus snaffling up kids who lean
+              over the barriers at the Natural History Museum.
+            </p>
+          </div>
         </div>
       </Wrapper>
     </React.Fragment>
